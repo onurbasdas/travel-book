80b42cb96f90dd700ae945ccb4df5adcd48ba0cf
onurbasdas <onurebasdase@hotmail.com>
2021-01-26 18:04:10 +0300

    project completed

--- TravelBook/ListViewController.swift
+++ TravelBook/ListViewController.swift
@@ -9,14 +9,11 @@ import UIKit
 import CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
     @IBOutlet weak var tableView: UITableView!
     var titleArray = [String]()
     var idArray = [UUID]()
     var chosenTitle = ""
-    var choseTitleId : UUID?
-    
+    var chosenTitleId : UUID?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,39 +25,54 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         getData()
     }
-    func getData(){
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newPlace"), object: nil)
+    }
+    
+    @objc func getData() {
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName:"Places")
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
         request.returnsObjectsAsFaults = false
-        do{
+        do {
             let results = try context.fetch(request)
-            if results.count > 0{
+            
+            if results.count > 0 {
+                
                 self.titleArray.removeAll(keepingCapacity: false)
                 self.idArray.removeAll(keepingCapacity: false)
                 
-                for result in results as! [NSManagedObject]{
-                    if let title = result.value(forKey: "title") as? String{
+                for result in results as! [NSManagedObject] {
+                    
+                    if let title = result.value(forKey: "title") as? String {
                         self.titleArray.append(title)
                     }
-                    if let id = result.value(forKey: "id") as? UUID{
+                    
+                    if let id = result.value(forKey: "id") as? UUID {
                         self.idArray.append(id)
                     }
+                    
                     tableView.reloadData()
+                    
                 }
+                
             }
-        }catch{
+            
+            
+        } catch {
             print("error")
         }
-       
         
     }
     
-    
-    @objc func addButtonClicked(){
+    @objc func addButtonClicked() {
         chosenTitle = ""
         performSegue(withIdentifier: "toViewController", sender: nil)
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return titleArray.count
     }
@@ -70,22 +82,21 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.textLabel?.text = titleArray[indexPath.row]
         return cell
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         chosenTitle = titleArray[indexPath.row]
-        choseTitleId = idArray[indexPath.row]
+        chosenTitleId = idArray[indexPath.row]
         performSegue(withIdentifier: "toViewController", sender: nil)
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "toViewController"{
+        if segue.identifier == "toViewController" {
             let destinationVC = segue.destination as! ViewController
             destinationVC.selectedTitle = chosenTitle
-            destinationVC.selectedTitleId = choseTitleId
+            destinationVC.selectedTitleID = chosenTitleId
+            
         }
     }
-    
-    
-    
-    
     
     
 }
